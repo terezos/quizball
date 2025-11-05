dad44e4191623e6f3b2f0ba2c0ef175722d28c64
georgeterezakis <georget@eight8.gr>
2025-11-05 22:18:58 +0200

    hide top5 category

--- app/Enums/QuestionType.php
+++ app/Enums/QuestionType.php
@@ -6,7 +6,7 @@ enum QuestionType: string
 {
     case TextInput = 'text_input';
     case MultipleChoice = 'multiple_choice';
-    case TopFive = 'top_5';
+//    case TopFive = 'top_5';
     case TextInputWithImage = 'text_input_with_image';
 
     public function label(): string
@@ -14,7 +14,7 @@ enum QuestionType: string
         return match($this) {
             self::TextInput => 'Συμπλήρωση κενού',
             self::MultipleChoice => 'Πολλαπλής επιλογής',
-            self::TopFive => 'Top 5',
+//            self::TopFive => 'Top 5',
             self::TextInputWithImage => 'Συμπλήρωση κενού με εικόνα',
         };
     }
